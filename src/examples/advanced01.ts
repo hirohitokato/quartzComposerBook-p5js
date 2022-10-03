652efbe445febf4955f79a48c28806016b4d5d3a
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2022-10-03 23:34:48 +0900

    Rotate cube

--- src/examples/advanced01.ts
+++ src/examples/advanced01.ts
@@ -3,6 +3,8 @@ import { QuartzComposition } from "../quartzComposition";
 import { Consumer } from "../quartz-composer/core/consumer";
 import { Image } from "../quartz-composer/provider/image";
 import { Cube } from "../quartz-composer/consumer/cube";
+import { Transformation3D } from "../quartz-composer/consumer/3DTransformation";
+import { Interpolation } from "../quartz-composer/provider/interpolation";
 
 let images: { [name: string]: Image } = {};
 
@@ -12,10 +14,26 @@ export class Advanced01 implements QuartzComposition {
   }
 
   setup(p: p5, consumers: Consumer[]) {
+    // top layer
+    let topComponent = new Transformation3D(p);
+    let xRotInterp = new Interpolation(p);
+    let yRotInterp = new Interpolation(p);
+    xRotInterp.startValue.setDefaultValue(0);
+    xRotInterp.endValue.setDefaultValue(360);
+    xRotInterp.duration.setDefaultValue(30);
+    yRotInterp.startValue.setDefaultValue(0);
+    yRotInterp.endValue.setDefaultValue(360);
+    yRotInterp.duration.setDefaultValue(40);
+    topComponent.xRotation.bind(xRotInterp.result);
+    topComponent.yRotation.bind(yRotInterp.result);
+    consumers.push(topComponent);
+
+    // second layer
     let cube = new Cube(p);
-    cube.width.setDefaultValue(3);
-    cube.height.setDefaultValue(3);
-    cube.depth.setDefaultValue(3);
+    topComponent.addConsumer(cube);
+    cube.width.setDefaultValue(3.5);
+    cube.height.setDefaultValue(3.5);
+    cube.depth.setDefaultValue(3.5);
     cube.frontImage.bind(images["wall"]!.image);
     cube.frontColor.setDefaultValue(p.color("#787F36"));
     cube.leftImage.bind(images["wall"]!.image);
@@ -28,6 +46,5 @@ export class Advanced01 implements QuartzComposition {
     cube.topColor.setDefaultValue(p.color("#820000"));
     cube.bottomImage.bind(images["wall"]!.image);
     cube.bottomColor.setDefaultValue(p.color("#820000"));
-    consumers.push(cube);
   }
 }
